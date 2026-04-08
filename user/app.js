83b587af6557448e6ce368d465c5b3c80fe1deff
Chandru S <chandru917231@gmail.com>
2026-04-08 16:26:51 +0530

    modified user/app.js

--- user/app.js
+++ user/app.js
@@ -1,5 +1,5 @@
 const http = require("http");
 http.createServer((req, res)=>{
     res.setHeader("Content-Type","text/plain; charset=utf-8");
-    res.end("User Service 👤");
+    res.end("User Service UPDATED 🚀");
 }).listen(4101);
